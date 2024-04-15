c4534d266fbc07596a8ce5cc86eae2f1fefb63d9
Pankaj <pankajmaurya1607@gmail.com>
2024-04-16 02:29:56 +0530

    updating socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mern-chat-app-master-prod.onrender.com/login", {
+			const socket = io("https://mern-chat-app-master-prod.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
