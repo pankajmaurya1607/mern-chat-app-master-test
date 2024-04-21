4d1d7b8e975553ca6daf167ef9d88ff0120dd295
Pankaj <pankajmaurya1607@gmail.com>
2024-04-21 22:30:12 +0530

    color and name change

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-				<h1 className='text-3xl font-semibold text-center text-gray-400'>
+				<h1 className='text-3xl font-semibold text-center text-gray-500'>
 					Login
 					<span className='text-blue-800'> BuzzTalk</span>
 				</h1>
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -26,7 +26,7 @@ const SignUp = () => {
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-				<h1 className='text-3xl font-semibold text-center text-gray-400'>
+				<h1 className='text-3xl font-semibold text-center text-gray-500'>
 					Sign Up <span className='text-blue-800'> BuzzTalk</span>
 				</h1>
 
